3eb63a17886537b39507a460cc27eff990371056
Farhan Faru <farhanfaru6458@gmail.com>
2026-02-14 14:54:36 +0530

    Small fix in develop

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,6 +10,7 @@ import Login from "../modules/auth/pages/Login";
 import Dashboard from "../modules/dashboard/pages/Dashboard";
 import Leads from "../modules/leads/pages/Leads";
 import Companies from "../modules/companies/pages/Companies";
+import CompanyDetails from "../modules/companies/pages/CompanyDetails";
 import Deals from "../modules/deals/pages/Deals";
 import Tickets from "../modules/tickets/pages/Tickets";
 
@@ -26,6 +27,7 @@ export default function AppRoutes() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/leads" element={<Leads />} />
         <Route path="/companies" element={<Companies />} />
+        <Route path="/companies/:id" element={<CompanyDetails />} />
         <Route path="/deals" element={<Deals />} />
         <Route path="/tickets" element={<Tickets />} />
       </Route>
